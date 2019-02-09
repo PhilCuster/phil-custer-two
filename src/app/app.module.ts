724c02c8762532eb46ba68ed9451067fcc13eedb
Phil Custer <custer55@gmail.com>
2019-02-09 17:38:19 -0500

    Added basic toolbar menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+// Angular Material
+import {
+  MatIconModule,
+  MatToolbarModule,
+} from '@angular/material';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './view/home/home.component';
+import { MenuComponent } from './view/menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    MenuComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FlexLayoutModule,
+
+    // Angular Material
+    MatIconModule,
+    MatToolbarModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
