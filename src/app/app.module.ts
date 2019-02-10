a6ac50a25d2946cb3d86700a834a71d03358bd51
Phil Custer <custer55@gmail.com>
2019-02-09 23:18:19 -0500

    Added pages and navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,31 +1,54 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FlexLayoutModule } from '@angular/flex-layout';
 
 // Angular Material
 import {
+  MatButtonModule,
   MatIconModule,
+  MatSidenavModule,
   MatToolbarModule,
 } from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './view/home/home.component';
 import { MenuComponent } from './view/menu/menu.component';
+import { AboutComponent } from './view/about/about.component';
+import { NotFoundComponent } from './view/not-found/not-found.component';
+import { ProjectsComponent } from './view/projects/projects.component';
+
+const appRoutes = [
+  { path: 'about', component: AboutComponent },
+  { path: '', component: HomeComponent, pathMatch: 'full' },
+  // TODO: Add 404 page.
+  { path: '**', component: NotFoundComponent },
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    MenuComponent
+    MenuComponent,
+    AboutComponent,
+    NotFoundComponent,
+    ProjectsComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: false } // debug routes
+    ),
     BrowserModule,
     BrowserAnimationsModule,
     FlexLayoutModule,
 
+
     // Angular Material
+    MatButtonModule,
     MatIconModule,
+    MatSidenavModule,
     MatToolbarModule,
   ],
   providers: [],
